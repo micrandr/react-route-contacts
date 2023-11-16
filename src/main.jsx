0626263b42a32ba018dcce7c85939caaffa4a136
Fy Dev <finoanamail@gmail.com>
2023-11-16 15:09:06 +0300

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,65 @@
+//https://reactrouter.com/en/main/start/tutorial
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Index from "./routes/index";
+import Root, { 
+  loader as rootLoader,
+  action as rootAction,
+} from './routes/root';
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from './routes/contact';
+import EditContact, {
+  action as editAction,
+} from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import ErrorPage from './error-page';
+import "./index.css";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            loader: contactLoader,
+            action: contactAction,
+            element: <Contact />
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ]
+      }
+  ]
+  },
+  
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
